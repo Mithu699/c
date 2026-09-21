e5815e82b272f12df39cd115d44584a5374223e9
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-09-22 02:38:14 +0530

    Add files via upload

---
+++ 02-chapter/index1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int b, c ;
+    b = c = 1;
+    int a = b+c;
+    int power = b^c;
+    printf("%d", power);
+
+    return 0;
+
+}
+
+// arithmetic instructions
+
+// modular operator 
+
+// return  remainder for int 
+
+// 3 % 2 = 1
+// -3 % 2 = -1
